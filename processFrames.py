a496e4596c9f36ed50a7922df33b8912f38bf796
Clayton Blythe <blythec1@central.edu>
2017-09-23 23:28:42 -0400

    working version with command line arguments, saving to ../wallpapers directory

--- processFrames.py
+++ processFrames.py
@@ -10,16 +10,20 @@ def get_args():
     parser = argparse.ArgumentParser(description='Script to convert movie file to a wallpaper')
     # Add arguments
     parser.add_argument('-f', '--movie_file', type=str, help='movie file name')
+    parser.add_argument('-w', '--width', type=int, help='width of wallpaper in pixels')
+    parser.add_argument('-v', '--height', type=int, help='height of wallpaper in pixels (number of pixels in vertical direction)')
     # Array for all arguments passed to script
     args = parser.parse_args()
     # Assign args to variables
     movie_file = args.movie_file
+    width = args.width
+    height = args.height
     # Return all variable values
-    return movie_file
+    return movie_file, width, height
 
  # Match return values from get_arguments()
  # and assign to their respective variables
-movie_file  = get_args()
+movie_file, width, height  = get_args()
 
 def make_frames(movie_file):
     os.system('ffmpeg -i {} -vf scale=8:8 im_%06d.png > /dev/null 2>&1'.format(movie_file))
@@ -27,17 +31,16 @@ def make_frames(movie_file):
 print('Converting movie to frames...please be patient. This may take up to a few minutes')
 make_frames(movie_file)
 
-new_size=(1,1080)
+new_size=(1,1)
 filenames = glob.glob('im*.png')
 total_width = len(filenames) * new_size[0]
 max_height = new_size[1]
 x_offset = 0
 
 new_im = Image.new('RGB', (total_width, max_height))
-print('Converting frames to wallpaper')
+print('Converting {} frames to wallpaper'.format(movie_file))
 for i in tqdm(range(1, len(filenames))):
-  new_size=(1,1080)
- # my_filename = 'time_%s.jpg' % (str(i+1))
+  new_size=(1,1)
   my_file= Image.open('im_%s.png' % str('%06d' % (i+1)))
   my_val = my_file.quantize(1).convert('RGB').getpixel((0,0))
   total_new_pixels = new_size[0] * new_size[1]
@@ -50,10 +53,9 @@ for i in tqdm(range(1, len(filenames))):
   my_file.close()
 
 current_dir = os.path.basename(os.getcwd())
-new_im.save(current_dir + '_finished_image.png')
+new_im.save('../wallpapers/' + '_finished_image.png')
 
-
-resize_ratio2 = (1920,1080)
+resize_ratio2 = (width,height)
 
 new_im2 = new_im.resize(resize_ratio2, Image.ANTIALIAS)
-new_im2.save(current_dir + '_finished_image_resized_1920,1080.png')
+new_im2.save('../wallpapers/' + 'finished_resized_{}x{}.png'.format(width, height))
